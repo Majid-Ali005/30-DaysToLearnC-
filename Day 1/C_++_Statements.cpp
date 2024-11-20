86459b5a7f5c6643cf5fb05dad741871e2ef78b7
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2024-11-20 15:53:05 +0000

    in this chapter we i am learn related about the c++ statements

---
+++ Day 1/C_++_Statements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //The following statement "instructs" the compiler to print the text "Hello World" to the screen:
+    cout << "Hello World";
+
+    //error tub dega jub aap semilon etc bhol jao
+    //For Example: cout << "Hello Word"  
+    //ye error dega q k aap ne end me semicolon nhi lagay hy.
+
+    //Many Statements
+
+    //Most C++ programs contain many statements.
+    //The statements are executed, one by one, in the same order as they are written:
+
+    cout << "hello World Statement One";
+    cout << "hello World Statements 2";
+}
