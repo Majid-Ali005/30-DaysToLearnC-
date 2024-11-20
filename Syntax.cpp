449f9ec1e5aa47aebe5c85d05583c222e77f212b
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2024-11-20 15:16:51 +0000

    ye aik whole syntax hy jiss ko aap ko bar bar likhna hi hoga warna aap ka prgoram run hi nhi hoga so that is very important.

---
+++ Syntax.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World";
+}
+
+// ye aik whole syntax hy jiss ko aap ko bar bar likhna hi hoga warna aap ka prgoram run hi nhi hoga so that is very important.
+
